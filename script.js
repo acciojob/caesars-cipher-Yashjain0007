645a25fc5eb7afeebdcd81c1fd7bfc333f215ec5
Yashjain0007 <124593014+Yashjain0007@users.noreply.github.com>
2023-03-17 16:18:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -28,8 +28,9 @@ function rot13(encodedStr){
 				}
 			decodedArr.push(decoded);
 		}
+	
 
-  return decodedArr;
+  return decodedArr.join(" ");
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
